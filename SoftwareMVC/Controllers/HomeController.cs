e2269d36bbf8936df3f6d5640d4b637c42f63ed8
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-09-28 13:23:03 -0300

    first commit

---
+++ SoftwareMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using SoftwareMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //Creamos una Instancia de nuestro ServicePelicula
+            var peliculaService = new PeliculasService();
+            var model=peliculaService.ObtenerPelicula();//Retorna un tipo Pelicula
+            var models = peliculaService.ObtenerPeliculas();
+            return View(models);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SoftwareMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftwareMVC.MvcApplication" Language="C#" %>
